e9092f06e49b9fd0f3e9cd5625ea4b62be73175e
Bernardo <bernardobraga2016@gmail.com>
2022-12-11 22:16:58 -0300

    Yarn added and more trash

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
+import Header from './Components/Header/index.js'
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bernardo gostoso 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header/>
   );
 }
 
-export default App;
+export default App; 
