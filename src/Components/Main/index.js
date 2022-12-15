974ceaa124a0ac6d94acc533dd9ff5bffc2b4ce0
Bernardo <bernardobraga2016@gmail.com>
2022-12-15 20:16:57 -0300

    Homepage main finished

---
+++ src/Components/Main/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {Body} from './styles.js'
+import Apontador from '../../assets/Apontador.jpg'
+import Borracha from '../../assets/Borracha.jpg'
+import Caneta_azul from '../../assets/Caneta_azul.jpg'
+import Caneta_vermelha from '../../assets/Caneta_vermelha.webp'
+import Ecolapis_de_cor from '../../assets/Ecolapis_de_cor.webp'
+import Ecolapis_grafite from '../../assets/Ecolapis_grafite.webp'
+import Lapiseira from '../../assets/Lapiseira.webp'
+import Caderno_1m from '../../assets/Caderno_1m.webp'
+import Caderno_10m from '../../assets/Caderno_10m.jpeg'
+import Caderno_anotacoes from '../../assets/Caderno_anotacoes.webp'
+import Caderno_u_1m from '../../assets/Caderno_u_1m.jpg'
+import Caderno_u_10m from '../../assets/Caderno_u_10m.webp'
+import Agenda from '../../assets/Agenda.jpg'
+import Mochila_anime from '../../assets/Mochila_anime.png'
+import Mochila_de_rodinha_b from '../../assets/Mochila_de_rodinha_b.png'
+import Mochila_de_rodinha_p from '../../assets/Mochila_de_rodinha_p.jpg'
+import Mochila_de_rodinha_r from '../../assets/Mochila_de_rodinha_r.png'
+import Mochila_lateral from '../../assets/Mochila_lateral.jpg'
+import Mochila_preta from '../../assets/Mochila_preta.jpg'
+import Mochila_rosa from '../../assets/Mochila_rosa.jpg'
+
+
+const Main = () => {
+    return (
+        <Body>              
+            <section id='materiais' class='materiais'>
+                <p><u>Buscar por</u>: materiais</p>
+                <img src={Apontador} class='img1' alt='Apontador Tilibra'/>
+                <img src={Borracha} class='img2' alt='Borracha Tilibra'/>
+                <img src={Caneta_azul} class='img3' alt='Caneta azul Bic'/>
+                <img src={Caneta_vermelha} class='img4' alt='Caneta vermelha Bic'/>
+                <img src={Ecolapis_de_cor} class='img5' alt='Ecolapis de cor Faber Castell'/>
+                <img src={Ecolapis_grafite} class='img6' alt='Ecolapis grafite Faber Castell'/>
+                <img src={Lapiseira} class='img7' alt='Lapiseira Faber Castell'/>
+            </section>
+            <section id='cadernos' class='cadernos'>
+                <p><u>Buscar por</u>: cadernos</p>
+                <img src={Caderno_1m} class='img1' alt='Caderno 1 matéria Bic'/>
+                <img src={Caderno_10m} class='img2' alt='Caderno 10 matérias Tilibra'/>
+                <img src={Caderno_anotacoes} class='img3' alt='Caderno de anotações Tilibra'/>
+                <img src={Caderno_u_1m} class='img4' alt='Caderno universitário 1 matéria Tilibra'/>
+                <img src={Caderno_u_10m} class='img5' alt='Caderno universitário 10 matérias Bic'/>
+                <img src={Agenda} class='img6' alt='Agenda Bic'/>
+            </section> 
+            <section id='mochilas' class='mochilas'>
+                <p><u>Buscar por</u>: mochilas</p>
+                <img src={Mochila_anime} class='img1' alt='Mochila anime Faber Castell'/>
+                <img src={Mochila_de_rodinha_b} class='img2' alt='Mochila de rodinha branca Bic'/>
+                <img src={Mochila_de_rodinha_p} class='img3' alt='Mochila de rodinha preta Tilibra'/>
+                <img src={Mochila_de_rodinha_r} class='img4' alt='Mochila de rodinha rosa Bic'/>
+                <img src={Mochila_lateral} class='img5' alt='Mochila lateral Faber Castell'/>
+                <img src={Mochila_preta} class='img6' alt='Mochila preta Tilibra'/>
+                <img src={Mochila_rosa} class='img7' alt='Mochila rosa Tilibra'/>
+            </section>      
+        </Body>
+    )
+}
+
+export default Main
---
+++ src/Components/Main/styles.js
@@ -0,0 +1,239 @@
+import styled from 'styled-components'
+
+export const Body = styled.div`
+        background-color: #E9E9E9;
+        display: flex;
+ 
+        .materiais {
+            height: 20rem;
+            width: 106rem;
+            margin: 7rem 7rem 0rem 7rem;
+            border-radius: 5px;
+            border-style: solid;
+            border-color: black;
+            background-color: white;
+        }
+
+        .materiais p{
+            font-family: inter;
+            font-size: 1.25rem; 
+            font-weight: bold;
+            padding-left: 1rem;
+        }
+
+        .materiais .img1{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 7rem 2rem;
+        }
+
+        .materiais .img2{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 7rem 2rem;
+        }
+
+        .materiais .img3{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 7rem 2rem;
+        }
+
+        .materiais .img4{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 7rem 2rem;
+        }
+
+        .materiais .img5{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 7rem 2rem;
+        }
+
+        .materiais .img6{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 7rem 2rem;
+        }
+
+        .materiais .img7{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 10rem 7rem 2rem;
+            position: absolute;
+        }
+
+        .cadernos {
+            height: 20rem;
+            width: 106rem;
+            margin: 31rem 0rem 0rem -113rem;
+            border-radius: 5px;
+            border-style: solid;
+            border-color: black;
+            background-color: white;
+        }
+            
+        .cadernos p{
+            font-family: inter;
+            font-size: 1.25rem; 
+            font-weight: bold;
+            padding-left: 1rem;
+        }
+
+        .cadernos .img1{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 7rem 2rem;
+        }
+
+        .cadernos .img2{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 7rem 2rem;
+        }
+
+        .cadernos .img3{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 7rem 2rem;
+        }
+
+        .cadernos .img4{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 7rem 2rem;
+        }
+
+        .cadernos .img5{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 7rem 2rem;
+        }
+
+        .cadernos .img6{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 7rem 2rem;
+        }
+
+        .mochilas {
+            height: 20rem;
+            width: 106rem;
+            margin: 55rem 0rem 7rem -106rem;
+            border-radius: 5px;
+            border-style: solid;
+            border-color: black;
+            background-color: white;
+        }
+
+        .mochilas p{
+            font-family: inter;
+            font-size: 1.25rem; 
+            font-weight: bold;
+            padding-left: 1rem;
+        }
+
+        .mochilas .img1{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 0rem 2rem;
+        }
+
+        .mochilas .img2{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 0rem 2rem;
+        }
+
+        .mochilas .img3{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 0rem 2rem;
+        }
+
+        .mochilas .img4{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 0rem 2rem;
+        }
+
+        .mochilas .img5{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 0rem 2rem;
+        }
+
+        .mochilas .img6{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 0rem 0rem 2rem;
+        }
+
+        .mochilas .img7{
+            height: 14rem;
+            width: 12rem;
+            border-style: solid;
+            border-color: black;
+            border-radius: 5px;
+            margin: 0rem 10rem 0rem 2rem;
+            position: absolute;
+        }
+`
